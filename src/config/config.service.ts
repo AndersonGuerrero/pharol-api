07e141f238245b2a4c98cf8e3650ad2796f77572
anderson <aguerrerodev@gmail.com>
2019-11-25 00:52:02 -0500

    Update

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,11 +1,12 @@
 import * as dotenv from 'dotenv';
 import * as fs from 'fs';
+import * as path from 'path';
 
 export class ConfigService {
   private readonly envConfig: Record<string, string>;
 
   constructor(filePath: string) {
-    this.envConfig = dotenv.parse(fs.readFileSync(filePath))
+    this.envConfig = dotenv.parse(fs.readFileSync(path.join(__dirname,'../..',filePath)));
   }
 
   get(key: string): string {
