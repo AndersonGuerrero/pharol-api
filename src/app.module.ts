0b81adbfc683b1864ec37db1cf87876753465f0f
anderson <aguerrerodev@gmail.com>
2019-11-25 00:07:12 -0500

    add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,22 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientModule } from './client/client.module';
+import { ConfigService } from './config/config.service';
+import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [
     ClientModule,
-    MongooseModule.forRoot(
-      'mongodb://localhost/pharol-api',
-      {
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
         useNewUrlParser: true,
         useUnifiedTopology: true
-      }
-    )
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule
   ],
   controllers: [AppController],
   providers: [AppService],
